3ccd9960f13cd5e65420bd4850e5ab8285a06c58
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2022-11-27 20:07:07 -0500

    Script CUDA

---
+++ cuda/script.sh
@@ -0,0 +1,19 @@
+echo ""
+echo "------------------------------------------------"
+echo "                     CUDA                       "
+echo "------------------------------------------------"
+echo ""
+echo "Compilando..."
+nvcc matMult.cu -o matMul -w
+
+#Realizar la ejecución de la multiplicación de matrices con distintos tamaños
+for n in {8,16,32,64,128,256,512,1024}
+do
+    for b in {0..10}
+    do
+        for t in {0..10}
+        do
+            ./matMult $n $((2**b)) $((2**t))
+        done
+    done
+done
